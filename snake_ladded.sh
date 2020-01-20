34f4c82e7dc5112956e5cc1fac7126a712dac8d7
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-01-20 10:33:53 +0530

    ADDED:check for option snake or ladder or no play

--- snake_ladded.sh
+++ snake_ladded.sh
@@ -5,4 +5,20 @@ postion=0;
 
 #UC-2
 rollNumber=$(( RANDOM%6 + 1 ))
-echo $rollNumber
+
+#UC-3 check for option
+noPlay=0;
+ladder=1;
+snake=2;
+option=$(( RANDOM%3 ))
+case $option in
+     $ladder)
+     echo "lader is came"
+     ;;
+     $snake)
+     echo "snake is came"
+     ;;
+     $noPlay)
+     echo "no play"
+     ;;
+esac
