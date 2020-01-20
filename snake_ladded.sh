470d95b3c8bd409e9aa89ff2ac0e00dca3221473
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-01-20 11:56:20 +0530

    ADDED:play with 2 player completed

--- snake_ladded.sh
+++ snake_ladded.sh
@@ -7,7 +7,12 @@ noPlay=0;
 ladder=1;
 snake=2;
 counter=0;
-while [ $position -lt $winPosition ]
+player1Score=0
+player2Score=0
+player1Chance=0
+player2Chance=1
+whoseChance=$player1Chance;
+while [ $player1Score -lt $winPosition -a $player2Score -lt $winPosition ]
 do
 #UC-2
 rollNumber=$(( RANDOM%6 + 1 ))
@@ -17,6 +22,13 @@ option=$(( RANDOM%3 ))
 case $option in
      $ladder)
 #UC-4
+     if [ $whoseChance -eq $player1Chance ]
+     then
+     position=$player1Score
+     else
+     position=$player2Score
+     fi
+
      if [ $(( position + rollNumber )) -le $winPosition ]
      then
      position=$(( position + rollNumber ));
@@ -33,5 +45,20 @@ case $option in
      echo "no play"
      ;;
 esac
-     Report[((counter++))]=$position
+    if [ $whoseChance -eq $player1Chance ]
+    then
+    player1Score=$position;
+    whoseChance=$player2Chance;
+    else
+    player2Score=$position;
+    whoseChance=$player1Chance;
+    fi
+    # Report[((counter++))]=$position
 done
+
+if [ $player1Score -eq $winPosition ]
+then
+   echo "player 1 win the match"
+else
+   echo "player 2 win the match"
+fi
